46b974a0f0c583db0c4d62f735cd0209c7c11ea5
piet <pprotassow@posteo.de>
2020-07-13 15:33:59 +0200

    table layout

--- resources/views/plz.blade.php
+++ resources/views/plz.blade.php
@@ -1,29 +1,44 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/css/app.css" rel="stylesheet">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Laravel</title>
 
-     
-    </head>
-    <body>
-    <h1>Autohändler</h1>
-    <div>
-        <h2>Orte</h2>
-        <ul>
-        
-        @foreach ($ausgabe as $ort)
-            <li>{{ $ort['dealer']}}: {{ $ort['distance']}} </li>
-        @endforeach
-            
-        </ul>
-       
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <div class="container-fluid w-50 p-3">
+        <div class="row justify-content-center">
+            <h1 class="pb-5 pt-5">Ergebnis der Umkreissuche</h1>
+        </div>
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th style="width: 45%" scope="col">Autohändler</th>
+                    <th style="width: 35%" scope="col">Ort</th>
+                    <th scope="col">Entfernung</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($ausgabe as $ort)
+                <tr>
+                    <th scope="row">{{ $ort['dealer']}}</th>
+                    <td>{{ $ort['ort']}}</td>
+                    <td>{{ $ort['distance']}} km</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 </body>
-    
+
 </html>
